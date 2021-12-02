1b5c443b37091a53603ee2c14322748b95521e8b
angel llompart <angelllompart@paucasesnovescifp.cat>
2021-12-02 17:25:35 +0100

    Creating bases part 2
    Created login dialog, forgot password dialog and user class to begin to work with.

---
+++ src/main/java/spdvi/User.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spdvi;
+
+/**
+ *
+ * @author angel
+ */
+public class User {
+    int id;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    String userName;
+    String email;
+    String password;
+
+    @Override
+    public String toString() {
+        return  id + ", " + userName + ", " + email + ", " + password ;
+    }
+    
+}
