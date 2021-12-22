f94140d2f28ad0b8d291b665bd529df78841fc46
joanandreu99 <joanandreumatas@paucasesnovescifp.cat>
2021-12-22 18:24:15 +0100

    Crear clase ConfirmPassword, falta modificarla

--- src/main/java/spdvi/ConfirmPassword.java
+++ src/main/java/spdvi/ConfirmPassword.java
@@ -4,11 +4,19 @@
  */
 package spdvi;
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Joan Andreu
  */
 public class ConfirmPassword extends javax.swing.JDialog {
+
+    private String email;
+    private String codi;
 
     /**
      * Creates new form ConfirmPassword
@@ -16,6 +24,22 @@ public class ConfirmPassword extends javax.swing.JDialog {
     public ConfirmPassword(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+    }
+
+    public String getCodi() {
+        return codi;
+    }
+
+    public void setCodi(String codi) {
+        this.codi = codi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     /**
@@ -27,23 +51,28 @@ public class ConfirmPassword extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        lblCodi = new javax.swing.JLabel();
+        txtCodi = new javax.swing.JTextField();
+        lblContrasenya = new javax.swing.JLabel();
+        lblConfirmContrasenya = new javax.swing.JLabel();
+        btnCodi = new javax.swing.JButton();
+        txtContrasenya = new javax.swing.JPasswordField();
+        txtConfirmContrasenya = new javax.swing.JPasswordField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
-        jLabel1.setText("Introdueix codic:");
+        lblCodi.setText("Introdueix codi:");
 
-        jLabel2.setText("Introdueix nova contrasenya:");
+        lblContrasenya.setText("Introdueix nova contrasenya:");
 
-        jLabel3.setText("Confirma la contrasenya:");
+        lblConfirmContrasenya.setText("Confirma la contrasenya:");
 
-        jButton1.setText("Cofirmar Codic");
+        btnCodi.setText("Cofirmar Codi");
+        btnCodi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCodiActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -53,43 +82,81 @@ public class ConfirmPassword extends javax.swing.JDialog {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jTextField1)
-                            .addComponent(jTextField3)
-                            .addComponent(jTextField2)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(lblCodi, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(txtConfirmContrasenya, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtContrasenya, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txtCodi, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(lblConfirmContrasenya, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(0, 0, Short.MAX_VALUE))
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(lblContrasenya, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(76, 76, 76)
-                        .addComponent(jButton1)
-                        .addGap(62, 62, 62))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                        .addComponent(btnCodi)
+                        .addGap(62, 62, 62))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(lblCodi)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtCodi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCodi, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel2)
+                .addComponent(lblContrasenya)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3)
+                .addComponent(txtContrasenya, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(12, 12, 12)
+                .addComponent(lblConfirmContrasenya)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txtConfirmContrasenya, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnCodiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCodiActionPerformed
+        String password = new String(txtContrasenya.getPassword());
+        
+        Pattern passwordRegEx = Pattern.compile("^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$");
+        if (passwordRegEx.matcher(password).matches()) {
+        
+        DataAccess da = new DataAccess();
+        ArrayList<User> users = da.getUsers();
+        boolean userExists = false;
+
+        if (codi.equals(txtCodi.getText())) {
+            JOptionPane.showMessageDialog(null, "El codi introduit es correcte"); 
+        } else {
+            JOptionPane.showMessageDialog(null, "El codi introduit esta malement");
+        }
+        
+        if(!new String(txtContrasenya.getPassword()).equals(new String(txtConfirmContrasenya.getPassword()))) {
+            JOptionPane.showMessageDialog(null, "La contrasenya no coincideix");
+        }
+
+        for (User u : users) {
+            if (u.getEmail().equals(email)) {
+                u.setPassword(new String(txtContrasenya.getPassword()));
+            }
+        }
+        } else if (!passwordRegEx.matcher(password).matches()) {
+            JOptionPane.showMessageDialog(null, "La contrasenya no cumpleix els minims requerits: " +
+                    "\n" +
+                    "Al menys 8 caracters\n" +
+                    "\n" +
+                    "Conte al menys un dígit\n" +
+                    "\n" +
+                    "Conte una Minuscula i una Mayuscula\n" +
+                    "\n" +
+                    "No conte espais, tabulacións, etc.");
+        }
+    }//GEN-LAST:event_btnCodiActionPerformed
 
     /**
      * @param args the command line arguments
@@ -134,12 +201,12 @@ public class ConfirmPassword extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JButton btnCodi;
+    private javax.swing.JLabel lblCodi;
+    private javax.swing.JLabel lblConfirmContrasenya;
+    private javax.swing.JLabel lblContrasenya;
+    private javax.swing.JTextField txtCodi;
+    private javax.swing.JPasswordField txtConfirmContrasenya;
+    private javax.swing.JPasswordField txtContrasenya;
     // End of variables declaration//GEN-END:variables
 }
